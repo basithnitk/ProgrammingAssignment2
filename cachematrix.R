ee819382522d5b7fc0d591d5079b1a8eb8a431b7
basithnitk <32350100+basithnitk@users.noreply.github.com>
2020-05-13 07:27:33 +0530

    Solution added
    The functions makeCacheMatrix and cacheSolve are completed.

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,32 @@
 ## functions do
 
 ## Write a short comment describing this function
+## The makeCacheMatrix function takes an atomic matrix and generates a special matrix with a list of functions
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set = function(y){
+    x<<-y
+    m<<-NULL
+  }
+  get = function() x
+  setinv = function(inv) m<<-inv
+  getinv = function() m
+  list(set=set,get=get,setinv=setinv,getinv=getinv)
 }
 
 
 ## Write a short comment describing this function
-
+## The cacheSolve function takes the list ouput from the makeCacheMatrix function and returns the inverse of the matrix in it
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m<- x$getinv()
+  if(!is.null(m)){
+    message("getting cached data")
+    return(m)
+  }
+  matrix <- x$get()
+  m <- solve(matrix,...)
+  x$setinv(m)
+  m
 }
